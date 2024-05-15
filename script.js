ed48adaef80952eacf94e037f6d69e7b9561329a
rabbierabbie <161629097+rabbierabbie@users.noreply.github.com>
2024-05-15 18:14:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let website=document.getElementById("website");
+let username=document.getElementById("username");
+let password=document.getElementById("password");
+
+//for masking the password
 function maskPassword(pass){
     let str = ""
     for (let index = 0; index < pass.length; index++) {
@@ -8,39 +13,58 @@ function maskPassword(pass){
 
 function copyText(txt) {
     navigator.clipboard.writeText(txt).then(
+        //The Window. navigator property refers to a Navigator object that contains information about the web browser as a whole, such as the version and a list of the data formats it can display. 
+
+        //clipboard.writetext() returns a promis which should be dealt with using then catch cases
         () => {
           /* clipboard successfully set */
-          document.getElementById("alert").style.display = "inline"
+          document.getElementById("alert").style.display = "inline"; //HTML DOM Element style
           setTimeout(() => {
             document.getElementById("alert").style.display = "none"
-          }, 2000);
+          }, 2000); //The setTimeout() is executed only once.
 
         },
         () => {
           /* clipboard write failed */
-          alert("Clipboard copying failed")
+          alert("Clipboard copying failed"); //The alert() method displays an alert box with a message and an OK button.
         },
       );
+      //Two arguments are given to the then() first which excutes if promise goes well, second if not
   }
 
 const deletePassword = (website)=>{
-    let data = localStorage.getItem("passwords")
+    let data = localStorage.getItem("passwords");//The getItem() method of the Storage interface, when passed a key name, will return that key's value, or null if the key does not exist, in the given Storage object.
+
+    //sessionStorage maintains a separate storage area for each given origin that's available for the duration of the page session (as long as the browser is open, including page reloads and restores).
+    //localStorage does the same thing, but persists even when the browser is closed and reopened.
+
+    //We need to note that Web Storage API stores storage objcets as key-value pairs and everything is stored as strings. Integers are automatically converted into strings.
+
     let arr = JSON.parse(data);
-    arrUpdated = arr.filter((e)=>{
+    //A common use of JSON is to exchange data to/from a web server. When receiving data from a web server, the data is always a string.Parse the data with JSON.parse(), and the data becomes a JavaScript object.
+
+    arrUpdated = arr.filter((e)=>{ // arrupdated is the new filtered array
+        //The filter() method creates a new array filled with elements that pass a test provided by a function.
         return e.website != website
     })
     localStorage.setItem("passwords", JSON.stringify(arrUpdated))
+    //Here, again we have to set the given item in localstorage but for that we need to convert everything to strings
     alert(`Successfully deleted ${website}'s password`)
-    showPasswords()
-
+    //The dollar sign followed by curly braces ${} is used to evaluate and embed expressions dynamically in template literals
+    showPasswords();
 }
 
 // Logic to fill the table
 const showPasswords = () => {
-    let tb = document.querySelector("table")
-    let data = localStorage.getItem("passwords")
-    if (data == null || JSON.parse(data).length == 0) {
-        tb.innerHTML = "No Data To Show"
+    //In regular functions the this keyword represented the object that called the function, which could be the window, the document, a button or whatever.
+
+    //With arrow functions the this keyword always represents the object that defined the arrow function.
+
+    let tb = document.querySelector("table");
+    let data = localStorage.getItem("passwords");
+    if (data == null || JSON.parse(data).length == 0)//important to parse here
+     {
+        tb.innerHTML = "No Data To Show"; // to set the innerhtml of that element
     }
     else {
         tb.innerHTML =  `<tr>
@@ -48,9 +72,12 @@ const showPasswords = () => {
         <th>Username</th>
         <th>Password</th>
         <th>Delete</th>
-    </tr> `
+    </tr> ` //use of backticks
+
+    //Template literals are literals delimited with backtick (`) characters, allowing for multi-line strings, string interpolation with embedded expressions, and special constructs called tagged templates.
+
         let arr = JSON.parse(data);
-        let str = ""
+        let str = "";
         for (let index = 0; index < arr.length; index++) {
             const element = arr[index];
 
@@ -73,24 +100,27 @@ const showPasswords = () => {
 }
 
 console.log("Working");
-showPasswords()
+
+showPasswords();
+
 document.querySelector(".btn").addEventListener("click", (e) => {
-    e.preventDefault()
-    console.log("Clicked....")
-    console.log(username.value, password.value)
+    e.preventDefault(); //This is essential here as otherwise a submission would be made immediately
+    //Also here ince in the index.html file we have mentioned required for all three fields the by default action of popping "required field" is also not done.
+    console.log("Clicked...");
+    console.log(username.value, password.value);
     let passwords = localStorage.getItem("passwords")
-    console.log(passwords)
+    console.log(passwords);
     if (passwords == null) {
-        let json = []
+        let json = [];
         json.push({website: website.value, username: username.value, password: password.value })
         alert("Password Saved");
         localStorage.setItem("passwords", JSON.stringify(json))
     }
     else {
-        let json = JSON.parse(localStorage.getItem("passwords"))
-        json.push({ website: website.value, username: username.value, password: password.value })
+        let json = JSON.parse(passwords);
+        json.push({ website: website.value, username: username.value, password: password.value });
         alert("Password Saved");
         localStorage.setItem("passwords", JSON.stringify(json))
     }
-    showPasswords()
-})
+    showPasswords();
+});
